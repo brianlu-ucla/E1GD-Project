bdd816060c961469ed2fc2065549b43a270f2f29
Salvador Cruz <salvador10@ucla.edu>
2025-03-06 14:37:51 -0800

    Added Menu Screen/Scene
    Finished the start button so when pressed it takes you to level 1. Working on the "Settings" and "Controls" windows and will have them done likely by the weekend.

---
+++ Assets/Scripts/OptionWindow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionWindow : MonoBehaviour
+{
+    public GameObject optionPanel;
+
+    void Start()
+    {
+        optionPanel.SetActive(false); // ensures that the option panel is hidden at start 
+    }
+
+    public void ToggleOptionPanel()
+    {
+        optionPanel.SetActive(!optionPanel.activeSelf); // Show/Hide the panel
+    }
+}
